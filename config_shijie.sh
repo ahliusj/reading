09725684757baa183a52e08f34f08635c57bdb46
shijieliu <shijieliu@localhost.localdomain>
2018-09-30 11:52:57 -0400

    update

--- config_shijie.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "alias l='ll'" >> ~/.bashrc
-echo "alias e='emacs'" >> ~/.bashrc
-echo "alias s='emacs -nw -Q'" >> ~/.bashrc
-echo "alias sd='sudo shutdown -h now'" >> ~/.bashrc
-echo "alias pandocref='pandoc --refrence-doc ref.docx " >> ~/.bashrc
